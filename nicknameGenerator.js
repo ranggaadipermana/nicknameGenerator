6003b12ffdb812ffbc82348920b2f020b506b797
ranggaadipermana <ranggaadi1998@gmail.com>
2024-10-21 19:55:22 +0700

    Update nicknameGenerator.js

--- nicknameGenerator.js
+++ nicknameGenerator.js
@@ -1,1 +1,50 @@
-function a() { var b = ["Dragon", "Shadow", "Dark", "Ninja", "Fire", "Thunder", "Ghost", "Steel", "Mystic", "Storm", "Phoenix", "Wolf", "Night", "Cyber", "Rogue", "Venom", "Flame", "Blade", "Alpha", "Omega", "Raven", "Vortex", "Titan", "Inferno", "Zephyr", "Silent", "Crimson", "Frost", "Void", "Nebula", "Star", "Tiger", "Wind", "Spirit", "Artifact", "Darkness", "Water", "Ocean", "Earth", "Flame", "Lightning", "Moon", "Queen", "Cobra", "Bolt", "Thunder", "Sky", "Nightfall", "Wave", "Dusk", "Cloud", "Shadow"], c = ["Slayer", "Master", "Warrior", "Hunter", "Blade", "Knight", "Rogue", "Guardian", "Wizard", "Assassin", "Reaper", "Champion", "Destroyer", "Berserker", "Ranger", "Invoker", "Commander", "Overlord", "Warden", "Sentry", "Predator", "Shifter", "Stormbringer", "Ghostwalker", "Chaosbringer", "Shadewalker", "Tempest", "Breaker", "Executor", "Saboteur", "Hero", "Vanquisher", "Protector", "Fighter", "Conqueror", "Avenger", "Savior", "Defender", "Champion", "Brawler", "Sorcerer", "Hunter", "Brawler", "Demon"], d = ["★", "⚡", "✿", "☯", "☠", "✧", "♡", "⚔", "♞", "♛", "∞", "✪", "✦", "⛧", "✡", "☾", "♢", "⍟", "✶", "☢", "☤", "♆", "✸", "⚜", "꩜", "𐙚", "•⩊•"], e = b[Math.floor(Math.random() * b.length)], f = c[Math.floor(Math.random() * c.length)], g = d[Math.floor(Math.random() * d.length)], h = g + " " + e + " " + f + " " + g, i = document.getElementById("fontSelect").value, j = document.getElementById("nicknameResult"); j.style.fontFamily = i; j.innerHTML = h; document.getElementById("copyButton").style.display = "inline-block"; } function copyNickname() { var a = document.getElementById("nicknameResult").textContent, b = document.createElement("textarea"); b.value = a; document.body.appendChild(b); b.select(); document.execCommand("copy"); document.body.removeChild(b); alert("Name " + a + " has been copied!"); }
+function a() {
+    var b = [
+        "Dragon", "Shadow", "Dark", "Ninja", "Fire", "Thunder", "Ghost", "Steel", 
+        "Mystic", "Storm", "Phoenix", "Wolf", "Night", "Cyber", "Rogue", "Venom", 
+        "Flame", "Blade", "Alpha", "Omega", "Raven", "Vortex", "Titan", "Inferno", 
+        "Zephyr", "Silent", "Crimson", "Frost", "Void", "Nebula", "Star", 
+        "Tiger", "Wind", "Spirit", "Artifact", "Darkness", "Water", "Ocean", 
+        "Earth", "Lightning", "Moon", "Cobra", "Bolt", "Glacier", "Titanium", 
+        "Obsidian", "Specter", "Wraith", "Fury", "Dusk", "Havoc", "Riptide"
+    ];
+    
+    var c = [
+        "Slayer", "Master", "Warrior", "Hunter", "Blade", "Knight", "Rogue", 
+        "Guardian", "Wizard", "Assassin", "Reaper", "Champion", "Destroyer", 
+        "Berserker", "Ranger", "Invoker", "Commander", "Overlord", "Warden", 
+        "Sentry", "Predator", "Shifter", "Stormbringer", "Ghostwalker", 
+        "Chaosbringer", "Shadewalker", "Tempest", "Breaker", "Executor", 
+        "Saboteur", "Hero", "Vanquisher", "Protector", "Fighter", "Conqueror", 
+        "Avenger", "Savior", "Defender", "Sentinel", "Prowler", "Brawler", 
+        "Juggernaut", "Demon", "Mystic"
+    ];
+    
+    var d = [
+        "★", "⚡", "✿", "☯", "☠", "✧", "♡", "⚔", "♞", "♛", "∞", "✪", "✦", 
+        "⛧", "✡", "☾", "♢", "⍟", "✶", "☢", "☤", "♆", "✸", "⚜", "꩜", 
+        "𐙚", "•⩊•"
+    ];
+    
+    var e = b[Math.floor(Math.random() * b.length)];
+    var f = c[Math.floor(Math.random() * c.length)];
+    var g = d[Math.floor(Math.random() * d.length)];
+    var h = g + " " + e + " " + f + " " + g;
+    var i = document.getElementById("fontSelect").value;
+    var j = document.getElementById("nicknameResult");
+    
+    j.style.fontFamily = i;
+    j.innerHTML = h;
+    document.getElementById("copyButton").style.display = "inline-block";
+}
+
+function copyNickname() {
+    var a = document.getElementById("nicknameResult").textContent;
+    var b = document.createElement("textarea");
+    b.value = a;
+    document.body.appendChild(b);
+    b.select();
+    document.execCommand("copy");
+    document.body.removeChild(b);
+    alert("Nama " + a + " berhasil disalin!");
+}
